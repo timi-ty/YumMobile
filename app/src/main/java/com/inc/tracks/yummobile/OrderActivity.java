c90718a76f7ea694b25469b9306ff86fabb808bf
Timi Tayo <timilehin.ty@gmail.com>
2020-01-14 16:51:34 +0100

    January 14 - 6 days to publish
    Signed-off-by: Timi Tayo <timilehin.ty@gmail.com>

--- app/src/main/java/com/inc/tracks/yummobile/OrderActivity.java
+++ app/src/main/java/com/inc/tracks/yummobile/OrderActivity.java
@@ -1,14 +1,20 @@
 package com.inc.tracks.yummobile;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.View;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -22,10 +28,18 @@ public class OrderActivity extends AppCompatActivity implements
 
     FragmentManager fragmentManager;
 
+    ConstraintLayout myLayout;
+
+    ProgressBar pbLoading;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        myLayout = findViewById(R.id.layout_orderActivity);
+
+        pbLoading = findViewById(R.id.pb_orderActivity);
 
         Toolbar myToolbar = findViewById(R.id.toolbar);
 
@@ -96,8 +110,10 @@ public class OrderActivity extends AppCompatActivity implements
 
     @SuppressWarnings("unchecked")
     private void confirmOrder(final HashMap<String, HashMap> orderGroups,
-                              HashMap<String, Integer> groupPrices,
-                              HashMap<String, String> groupDescs){
+                              final HashMap<String, Integer> groupPrices,
+                              final HashMap<String, String> groupDescs){
+        setLoadingUi(true);
+
         FirebaseFirestore fireDB = FirebaseFirestore.getInstance();
 
         ArrayList<String> restaurantIds = new ArrayList<>(orderGroups.keySet());
@@ -123,9 +139,36 @@ public class OrderActivity extends AppCompatActivity implements
                             .addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
+                                    setLoadingUi(false);
                                     goToOrderCompleteFragment(orderGroups);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Snackbar snackbar = Snackbar.make(myLayout,
+                                            "Order could not be made", Snackbar.LENGTH_LONG)
+                                            .setActionTextColor(getResources()
+                                                    .getColor(R.color.colorPrimaryDark));
+
+                                    snackbar.setAction("Try Again", new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            confirmOrder(orderGroups, groupPrices, groupDescs);
+                                        }
+                                    });
+
+                                    setLoadingUi(false);
                                 }
                             });
         }
+    }
+
+    private void setLoadingUi(boolean loading){
+        myLayout.setEnabled(!loading);
+
+        pbLoading.setVisibility(loading ? View.VISIBLE : View.INVISIBLE);
+
+        myLayout.setAlpha(loading ? 0.4f : 1.0f);
     }
 }
